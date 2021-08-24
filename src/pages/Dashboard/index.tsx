52751376b4b753614df9d0d90612b025fea1d440
eliveltonsf <elivelton.seven@gmail.com>
2021-08-24 11:17:33 -0300

    feat: creating dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ContentHeader from '../../components/ContentHeader'
+import SelectInput from '../../components/SelectInput';
+
+import { Container } from './styles'
+
+
+const Dashboard: React.FC = () => {
+
+  const options = [
+    { value: 'Elivelton', label: 'Rodrigo' },
+    { value: 'Maria', label: 'Maria' },
+    { value: 'Ana', label: 'Ana' }
+  ]
+
+  return (
+    <Container>
+      <ContentHeader title="Dashboard" lineColor="#f7931b">
+        <SelectInput options={options} onChange={() => { }} />
+
+      </ContentHeader>
+    </Container>
+  )
+}
+
+export default Dashboard;
