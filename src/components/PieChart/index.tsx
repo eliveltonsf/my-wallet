de2d993d39f097597d012902767cccbf2571a1c9
eliveltonsf <elivelton.seven@gmail.com>
2021-08-26 15:08:58 -0300

    feat: creating PieChart component

---
+++ src/components/PieChart/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import {  Pie, Cell, ResponsiveContainer } from 'recharts';
+
+import { Container, SideLeft, SideRight, LegendContainer, Legend } from './styles';
+
+const PieChart: React.FC = () => (
+  <Container>
+    <SideLeft>
+    <h2>Relação</h2>
+      <LegendContainer>
+     
+        <Legend color="#F7931B"> 
+          <div>5%</div>
+          <span>Entradas</span>
+        </Legend>
+
+        <Legend color="#E44C4E">
+          <div>95%</div>
+          <span>Saídas</span>
+        </Legend>
+
+      </LegendContainer>
+    </SideLeft>
+
+    <SideRight>
+      {/* <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={[{amount: 30, percent: 95}]}
+            labelLine={false}
+            dataKey="percent"
+          >
+            {}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer> */}
+    </SideRight>
+  </Container>
+);
+
+export default PieChart;
