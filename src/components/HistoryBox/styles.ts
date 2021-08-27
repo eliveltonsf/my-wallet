8546994462010c7feaf3d0f7d71fb3d90b461af3
eliveltonsf <elivelton.seven@gmail.com>
2021-08-27 01:07:16 -0300

    feat: creating HistoryBox component

---
+++ src/components/HistoryBox/styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+interface ILendendProps {
+  color: string;
+}
+
+export const Container =  styled.div`
+  width: 100%;
+  height: 340px;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: ${props => props.theme.colors.tertiary};
+  color: ${props => props.theme.colors.white};
+
+  margin: 10px 0;
+  padding: 30px 20px;
+
+  border-radius: 7px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+
+  >h2{
+      margin-bottom: 20px;
+      padding-left: 18px;
+    }
+`;
+
+export const LegendContainer =  styled.ul`
+  list-style: none;
+  display: flex;
+
+  max-height: 175px;
+  padding-right:15px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb{
+   background-color: ${props => props.theme.colors.secondary};
+   border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-track{
+   background-color: ${props => props.theme.colors.tertiary};
+  }
+`;
+
+export const Legend =  styled.li<ILendendProps>`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 7px;
+  margin-left: 7px;
+
+  >div{
+    background-color: ${props=> props.color};
+
+    width: 40px;
+    height: 40px;
+    border-radius: 5px;
+
+    font-size: 14px;
+    line-height: 40px;
+    text-align: center;
+  }
+
+  >span{
+    margin-left: 5px;
+  }
+`;
+
+export const ChartContainer =  styled.div`
+   flex: 1;
+    height: 260px;
+`;
