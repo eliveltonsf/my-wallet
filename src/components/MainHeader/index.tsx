770b7a8aea108c1923a810f01a38d4511c0e3541
eliveltonsf <elivelton.seven@gmail.com>
2021-09-02 11:09:18 -0300

    feat: using toggle component to switch themes

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,6 +1,8 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import emojis from '../../utils/emojis'
+
+import {useTheme} from '../../hooks/theme'
 
 import Toggle from "../Toggle";
 
@@ -13,15 +15,28 @@ import {
 
 const MainHeader: React.FC = () => {
 
+  const {toggleTheme, theme} = useTheme();
+
+  const [darkTheme, setDarkTheme] = useState(()=> theme.title === 'dark' ? true : false)
+
   const emoji = useMemo(() => {
     const indice = Math.floor(Math.random() * emojis.length);
     return emojis[indice]
   }, []);
 
+  const  handleChangeTheme = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  }
 
   return (
     <Container>
-      <Toggle />
+      <Toggle 
+      labelLeft="Light"
+      labelRight="Dark"
+      checked={darkTheme}
+      onChange={handleChangeTheme}
+      />
 
       <Profile>
         <Welcome>Olá,{emoji}</Welcome>
