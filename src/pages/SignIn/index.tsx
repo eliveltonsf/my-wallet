5746aca0531fc7ef382fe1fe06ec0fb290fe6177
eliveltonsf <elivelton.seven@gmail.com>
2021-10-06 14:08:31 -0300

    feat: implementing signin page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import logoimg from '../../assets/logo.svg';
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Logo, Form, FormTitle } from './styles';
+
+import {useAuth} from '../../hooks/auth'
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const {singIn} = useAuth();
+
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      <Logo>
+        <img src={logoimg} alt="Minha Carteira"/>
+        <h2>Minha Carteira</h2>
+      </Logo>
+
+      <Form onSubmit={() => singIn(email,password)}>
+        <FormTitle>Entrar</FormTitle>
+
+        <Input type="email" placeholder="e-mail" onChange={(e) => setEmail(e.target.value)} required />
+        <Input  type="password" placeholder="senha" onChange={(e) => setPassword(e.target.value)} required/>
+        <Button type="submit">Acessar</Button>
+      </Form>
+    </Container>
   )
 }
 
