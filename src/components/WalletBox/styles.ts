3c4e0d20b3e3d481ee2beabba98f63f06a08caed
eliveltonsf <elivelton.seven@gmail.com>
2021-10-06 14:12:32 -0300

    feat: add authprovider in project root

--- src/components/WalletBox/styles.ts
+++ src/components/WalletBox/styles.ts
@@ -16,7 +16,7 @@ export const Container = styled.div<IContainerProps>`
   position: relative;
   overflow: hidden ;
 
-  >img{
+  > img{
     top: -10px;
     right: -28px;
     height: 110%;
@@ -24,14 +24,58 @@ export const Container = styled.div<IContainerProps>`
     opacity: 30%;
   }
 
-  >span{
+  > span{
     font-size: 18px;
     font-weight: 500;
   }
 
-  >small{
+  > small{
     bottom: 10px;
     font-size: 12px;
     position: absolute;
+  }
+
+  > strong::after{
+        content:' ';
+        width: 7px;
+        display:inline-block;
+  }
+
+  @media(max-width: 770px){
+    > span {
+      font-size: 14px;
+    }
+
+    > h1 {
+      word-wrap: break-word;
+      font-size: 22px;
+
+      > strong {
+        display: inline-block;
+        width: 100%;
+        font-size: 12px;
+      }
+    }    
+  }
+
+  @media(max-width: 420px){
+    width: 100%;
+
+    > h1 {
+      display: flex;
+
+      strong{
+        position: initial;
+        width: auto;
+        font-size: 22px;        
+      }
+
+      strong::after{
+        content:' ';
+        width: 1px;
+        display:inline-block;
+      }
+    }   
+    
   }
 `;
